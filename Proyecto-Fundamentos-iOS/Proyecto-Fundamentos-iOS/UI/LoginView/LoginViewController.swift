1cc7f439d05b7d53dfb5a9ba858e02863cd31d89
Eduardo Martinez <edugproduce@gmail.com>
2022-12-30 19:26:58 +0100

    Añado archivo LoginView falta editar y enlazar

---
+++ Proyecto-Fundamentos-iOS/Proyecto-Fundamentos-iOS/UI/LoginView/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  Proyecto-Fundamentos-iOS
+//
+//  Created by Eduardo Martinez Palomino on 30/12/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
